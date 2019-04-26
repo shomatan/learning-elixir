24f93c88a5f7fb0cad6fba961f878f996393b4b8
Shoma Nishitateno <shoma416@gmail.com>
2019-04-27 02:46:15 +0900

    fix: don't do live-reloading

--- app/config/dev.exs
+++ app/config/dev.exs
@@ -51,7 +51,8 @@ config :app, AppWeb.Endpoint,
     patterns: [
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
-      ~r"lib/app_web/{live,views}/.*(ex)$",
+      ~r"lib/app_web/views/.*(ex)$",
+      ~r{lib/app_web/live/.*(ex)$},
       ~r"lib/app_web/templates/.*(eex)$"
     ]
   ]
